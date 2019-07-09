910a045f92edcbd37eee87f6fd1cd46c8f061823
Kiattirat Sujjapongse <keittirat@ekoapp.com>
2019-07-10 01:32:03 +0700

    fix typo

--- resources/views/debug.blade.php
+++ resources/views/debug.blade.php
@@ -7,7 +7,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h1 class="text-center" style="padding: 50px 0">DEBUGING - {{$title}}</h1>
+                    <h1 class="text-center" style="padding: 50px 0">DEBUGGING - {{$title}}</h1>
                 </div>
             </div>
         </div>
